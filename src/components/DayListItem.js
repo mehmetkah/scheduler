020ee18183afe777c8ed322d215e3c62733555b4
“mehmetkah” <mehmetkahca@gmail.com>
2022-11-06 17:19:14 -0700

    initial commit

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,17 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  function formatSpots() {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`;
+    }
+  }
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
@@ -11,7 +22,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
