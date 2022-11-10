08198b7479b92b8ccb936a2661c913b684962a49
“mehmetkah” <mehmetkahca@gmail.com>
2022-11-09 23:04:44 -0700

    All appointment components are created

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "components/Appointment";
 import "components/Appointment/styles.scss";
+import Confirm from "./Confirm";
 import Header from "./Header";
-
+import Empty from "./Empty";
+import Show from "./Show";
 export default function Appointment(props) {
   return <article className="appointment"></article>;
 }
--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,11 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 import "index.scss";
 import DayList from "components/DayList";
 import DayListItem from "components/DayListItem";
@@ -130,4 +135,28 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+      on
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      message="Delete the appointment?"
+      onCancel={action("onCancel")}
+      onConfirm={action("onConfirm")}
+    />
+  ))
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => (
+    <Error
+      message="Could not delete appointment."
+      onClose={action("onClose")}
+    />
+  ));
